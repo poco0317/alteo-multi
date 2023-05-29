6da616c341c7c5af5f2ee71d3f846a21c232177c
Barinade <poco0317@users.noreply.github.com>
2023-05-29 16:36:56 -0500

    move the things out

--- src/main/java/com/etterna/multi/repo/LobbyMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.etterna.multi.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.etterna.multi.data.LobbyMessage;
-
-public interface LobbyMessageRepository extends JpaRepository<LobbyMessage, Long> {
-
-}
--- src/main/java/com/etterna/multi/repo/LobbyScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.etterna.multi.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.etterna.multi.data.LobbyScore;
-
-public interface LobbyScoreRepository extends JpaRepository<LobbyScore, Long> {
-
-}
